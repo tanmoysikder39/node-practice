b396a7ff6d816c64af9ddebe903d5b3575b3cc68
Tanmoy Sikder <tanmoysikder39@gmail.com>
2023-06-01 17:38:32 +0600

    Server Setup

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
-console.log('Hello World');
-console.log('Hello World');
+const express = require('express');
+const server = express();
+const port = 5000;
+
+server.get('/', (req, res) => {
+    res.send('Hello from Express!');
+    }
+);
+
+server.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+    }
+);
